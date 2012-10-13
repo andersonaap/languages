87c1ff49f3a47a44e355fe16fa77ee91df49c1f9
anderson <anderson@MARS03.(none)>
2012-10-13 16:42:42 -0300

    cb click

--- javascript/dom_manipulation/jquery/todo.js
+++ javascript/dom_manipulation/jquery/todo.js
@@ -3,12 +3,19 @@ $(document).ready(function () {
 
     console.log("ready");
 
-    $("#texto").keyup(function (event) {
-        if(event.which == 13) { // ou event.keyCode
-            event.preventDefault();
+    $("#texto").keyup(function (evento) {
+        if(evento.which == 13) { // ou evento.keyCode
+            evento.preventDefault();
             var texto = $("#texto").val();
             $("#texto").val("");
-            $("#lista").append('<li><label>' + texto + '</label><input type="checkbox"></li>')      
+            $("#lista").append('<li><label>' + texto + '</label><input type="checkbox"></li>');
+
+
+            $("#lista input:last").click(function() {
+                var checked = this.checked;
+                console.log("onChecked: " + checked);
+            });
         }
     });
-})
+});
+
