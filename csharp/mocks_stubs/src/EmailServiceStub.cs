faa75be84c8b498b0393072e947fb0b3ca04f435
Anderson Pavia <andersonaap@hotmail.com>
2012-10-16 04:33:55 -0300

    stub

---
+++ csharp/mocks_stubs/src/EmailServiceStub.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mocks_stubs
+{
+    public class EmailServiceStub : IEmailService
+    {
+        private IList<string> _mensagens = new List<string>();
+
+        public void Enviar(string mensagem)
+        {
+            _mensagens.Add(mensagem);
+        }
+        
+        public int QuantidadeEnviada()
+        {
+            return _mensagens.Count;
+        }
+    }
+}
---
+++ csharp/mocks_stubs/src/IEmailService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mocks_stubs
+{
+    public interface IEmailService
+    {
+        void Enviar(string mensagem);
+        int QuantidadeEnviada();
+    }
+}
