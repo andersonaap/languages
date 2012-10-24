97fa3d8c02586346a17a185615b176dd17e15f27
Anderson Pavia <andersonaap@hotmail.com>
2012-10-24 00:03:54 -0200

    first test

---
+++ csharp/ecomerce_ddd/Ecommerce.Domain/Produto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ecommerce.Domain
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
