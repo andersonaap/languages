6edebd5e4d75bf1546cd15fb2362693c41b5bda2
Anderson Pavia <andersonaap@hotmail.com>
2012-10-24 00:30:55 -0200

    rename

---
+++ csharp/ecommerce_ddd/Ecommerce.Domain.Tests/PesquisaDeProdutosServiceTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Moq;
+using NUnit.Framework;
+
+using Ecommerce.Domain;
+
+
+namespace Ecommerce.Domain.Tests
+{
+    [TestFixture]
+    public class CarrinhoDeComprasServiceTest
+    {
+        private IEnumerable<Produto> produtosIniciais;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            produtosIniciais =
+                new Produto[]
+                {
+                    new Produto { Id = 1, Nome = "SmartTv" },
+                    new Produto { Id = 2, Nome = "Ultrabook" },
+                    new Produto { Id = 3, Nome = "Smartphone" },
+                }
+                .AsEnumerable();
+        }
+
+        [Test]
+        public void DeveListarTodosOsProdutos()
+        {
+            var produtoRepositoryStub = new Mock<IProdutoRepository>(MockBehavior.Strict);
+
+            produtoRepositoryStub
+                .Setup(x => x.GetAll())
+                .Returns(produtosIniciais);
+                
+            var carrinho = new PesquisaDeProdutosService(produtoRepositoryStub.Object);
+
+            var produtosObtidos = carrinho.ListarProdutos();
+
+            CollectionAssert.AreEqual(produtosObtidos, produtosIniciais);
+        }
+    }
+}
---
+++ csharp/ecommerce_ddd/Ecommerce.Domain/PesquisaDeProdutosService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ecommerce.Domain
+{
+    public class PesquisaDeProdutosService
+    {
+        private IProdutoRepository _produtoRepository;
+
+        public PesquisaDeProdutosService(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        public IEnumerable<Produto> ListarProdutos()
+        {
+            return _produtoRepository.GetAll();
+        }
+    }
+}
