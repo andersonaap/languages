8b852f8d9481c6119f1a5880e19946cb43401a64
Anderson Pavia <andersonaap@hotmail.com>
2012-10-24 01:02:21 -0200

    snd test

---
+++ csharp/ecommerce_ddd/Ecommerce.Domain/CarrinhoDeComprasService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ecommerce.Domain
+{
+    public class CarrinhoDeComprasService
+    {
+        public IList<ItemDePedido> ItensDePedido { get; set; }
+
+        public CarrinhoDeComprasService()
+        {
+            ItensDePedido = new List<ItemDePedido>();
+        }
+    }
+}
---
+++ csharp/ecommerce_ddd/Ecommerce.Domain/ItemDePedido.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Domain
+{
+    public class ItemDePedido
+    {
+        public int Id { get; set; }
+        public Produto Produto { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
