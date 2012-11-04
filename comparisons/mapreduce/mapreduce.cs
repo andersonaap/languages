24d1b363eab7d90a248f6ee6980827bd1e032471
anderson pavia <andersonaap@hotmail.com>
2012-11-04 16:14:54 -0200

    csharp

---
+++ comparisons/mapreduce/mapreduce.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var somaDosParesAoQuadrado =
+            Enumerable.Range(1, 100)
+                .Where(x => x % 2 == 0)
+                .Select(x => x * x)
+                .Aggregate((acc, x) => acc + x);
+
+        Console.WriteLine(somaDosParesAoQuadrado);
+    }
+}
