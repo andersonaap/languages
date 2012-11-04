79b24899d181e025b728a879a86282714ee81fb6
anderson pavia <andersonaap@hotmail.com>
2012-11-04 16:15:09 -0200

    fsharp

---
+++ comparisons/mapreduce/mapreduce.fs
@@ -0,0 +1,10 @@
+﻿
+let somaDosParesAoQuadrado = 
+    [1..100]
+    |> Seq.filter (fun x     -> x % 2 = 0 )
+    |> Seq.map    (fun x     -> x * x)
+    |> Seq.reduce (fun acc x -> acc + x)
+
+printfn "%A" somaDosParesAoQuadrado
+
+
