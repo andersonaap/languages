bc8e96e06706a58258e11d90d46b13031a46cc07
anderson pavia <andersonaap@hotmail.com>
2012-11-04 16:15:38 -0200

    vbnet

---
+++ comparisons/mapreduce/mapreduce.vb
@@ -0,0 +1,15 @@
+﻿Module mapreduce
+
+    Sub Main()
+
+        Dim somaDosParesAoQuadrado =
+            Enumerable.Range(1, 100) _
+                .Where(Function(x) x Mod 2 = 0) _
+                .Select(Function(x) x ^ 2) _
+                .Aggregate(Function(acc, x) acc + x)
+
+        Console.WriteLine(somaDosParesAoQuadrado)
+
+    End Sub
+
+End Module
