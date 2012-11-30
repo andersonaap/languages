e70314a27698c44b57eb4fb7584be4210e2cc2e3
andersonaap <andersonaap@hotmail.com>
2012-11-30 00:20:10 -0200

    14-17

---
+++ haskell/99_question/problems_11-20.hs
@@ -0,0 +1,36 @@
+--
+-- http://www.haskell.org/haskellwiki/99_questions/11_to_20
+--
+
+
+-- Problem 14
+-- Duplicate the elements of a list.
+dupli =  foldl f []
+    where f acc x = acc ++ replicate 2 x
+
+-- Problem 15
+-- Replicate the elements of a list a given number of times.
+repli xs qt = foldl f [] xs
+    where f acc x = acc ++ replicate qt x 
+
+-- Problem 16
+-- Drop every N'th element from a list.
+dropEvery xs n | length xs > n = take (n - 1) xs ++ dropEvery (drop n xs) n
+               | otherwise     = take (n - 1) xs
+
+-- Problem 17
+-- Split a list into two parts; the length of the first part is given.
+-- Do not use any predefined predicates.
+split xs n = f (xs, "")
+    where f (a1, a2) | length a1 > n = f (init a1, last a1:a2)
+                     | otherwise     = (a1, a2)
+
+
+main = do
+    print $ dupli [1, 2, 3]
+    print $ repli "abc" 3
+    print $ dropEvery "abcdefghik" 3
+    print $ split "abcdefghik" 3
+
+
+
