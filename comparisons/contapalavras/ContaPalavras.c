fb43c4b5a0ae7c6f229f72c7078134e5bfec2237
Anderson Pavia <andersonaap@hotmail.com>
2013-05-23 09:40:54 -0300

    usando isalpha do ctype.h

--- comparisons/contapalavras/ContaPalavras.c
+++ comparisons/contapalavras/ContaPalavras.c
@@ -1,51 +1,45 @@
 #include <stdio.h>
- 
-#define BOOL int
+#include <ctype.h>
+
+
 #define TRUE 1
-#define FALSE 0
- 
- 
-BOOL ehLetra(const char caracter) {
-	char caracterCaixaBaixa = 0x20 | caracter;
-	if(caracterCaixaBaixa >= 'a' && caracterCaixaBaixa <= 'z')
-		return TRUE;
-	return FALSE;
-}
- 
+
+
+
 int contarPalavras(const char * frase) {
- 
+
 	int quantidadePalavras = 0;
 	int posicaoCaracter = 0;
 	char caracterAnterior = ' ';
- 
+
 	while(TRUE) {
 		char caracterAtual = frase[posicaoCaracter];
- 
+
 		if(caracterAtual == '\0')
 			break;
- 
-		if(!ehLetra(caracterAnterior) && ehLetra(caracterAtual))
+
+		if(!isalpha(caracterAnterior) && isalpha(caracterAtual))
 			quantidadePalavras++;
- 
+
 		caracterAnterior = caracterAtual;
 		posicaoCaracter++;
 	}
- 
+
 	return quantidadePalavras;
 }
- 
+
 int main() {
 	char frase[1024];
 	int quantidadePalavras;
- 
+
 	printf("Digite uma frase\n");
- 
+
 	// http://stackoverflow.com/questions/6282198/reading-string-from-input-with-space-character
 	scanf("%[^\n]s", frase); 
  
 	quantidadePalavras = contarPalavras(frase);
  
 	printf("A frase tem %d palavras.\n", quantidadePalavras);
- 
+
 	return 0;
 }
