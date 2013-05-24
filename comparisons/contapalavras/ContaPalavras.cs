f7069c8076e9a6fc9281cc9e5b5a59392c8d8b42
Anderson Pavia <andersonaap@hotmail.com>
2013-05-24 08:52:30 -0300

    csharp

---
+++ comparisons/contapalavras/ContaPalavras.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite uma frase.");
+        var frase = Console.ReadLine();
+        var quantidadePalavras = ContaPalavras(frase);
+        Console.WriteLine("A frase tem {0} palavreas", quantidadePalavras);
+    }
+
+    private static int ContaPalavras(string frase)
+    {
+        var quantidadePalavras = 0;
+        var caracterAnterior = ' ';
+        foreach (var caracterAtual in frase)
+        {
+            if (!char.IsLetter(caracterAnterior) && char.IsLetter(caracterAtual))
+                quantidadePalavras++;
+
+            caracterAnterior = caracterAtual;
+        }
+        return quantidadePalavras;
+    }
+}
