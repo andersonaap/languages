ab1b2ecce1c96c3ccd29f2847d2957ad7c68d538
Anderson Pavia <andersonaap@hotmail.com>
2015-03-27 04:50:23 -0700

    Criacao de um site MVC5 utilizando o pacote "StructureMap.MVC5"

---
+++ csharp/structuremap/SiteMvc/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Implementacoes;
+using Interfaces;
+
+
+namespace SiteMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ITipoC _tipoC;
+
+        public HomeController(ITipoC tipoC)
+        {
+            _tipoC = tipoC;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ csharp/structuremap/SiteMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SiteMvc.MvcApplication" Language="C#" %>
